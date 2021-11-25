1f60e93df1e758f3f4d1e47689b0bd66ffa71971
vhuynh100 <80740266+vhuynh100@users.noreply.github.com>
2021-11-25 00:29:12 -0600

    Required WOKCommands and updated .gitignore

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 const Discord = require('discord.js');
 require('dotenv').config();
+const WOKCommands = require('wokcommands')
+
 const client = new Discord.Client();
 
 const prefix = '-j '
-
-// client.commands = new Discord.Collection();
-// client.events = new Discord.Collection();
-
-// ['command_handler', 'event_handler'].forEach(handler => {
-//     require(`./handlers/${handler}`)(client, Discord);
-// });
 
 client.once('ready', () => {
     console.log('JLCC is online!');
