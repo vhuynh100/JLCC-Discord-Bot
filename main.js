8fbf934ef2d1306e4970de020b6d5d9a2267c066
vhuynh100 <80740266+vhuynh100@users.noreply.github.com>
2021-12-02 18:34:32 -0600

    Set up WOKCommands and acquired Mongo URI

--- main.js
+++ main.js
@@ -5,20 +5,25 @@ const WOKCommands = require('wokcommands')
 const client = new Discord.Client();
 
 const config = require('./config.json')
-const mongo = require('./mongo')
+// const mongo = require('./mongo')
 
 const prefix = '-j '
 
-client.once('ready', async () => {
+client.once('ready',  () => {  //add async back in before ()
     console.log('JLCC is online!');
 
-    await mongo().then(mongoose => {
-        try {
-            console.log('Connected to mongo!')
-        } finally {
-            mongoose.connection.close()
-        }
-    })
+    // await mongo().then(mongoose => {
+    //     try {
+    //         console.log('Connected to mongo!')
+    //     } finally {
+    //         mongoose.connection.close()
+    //     }
+    // })
+
+    new WOKCommands(client, {
+        commandsDir: 'commands',
+        mongoUri: process.env.MONGO_URI
+    }) //.setMongoPath(process.env.MONGO_URI)
 });
 
 client.on('message', message =>{
