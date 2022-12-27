e0db67aa9598b30264fa578af5f64eebb4300319
Vanessa <v.huynh.a@gmail.com>
2022-12-27 13:47:51 -0600

    Changed channelID arg to channelTag, changed new message content to be collected instead of it being a single-word input, added handlers for incorrect channel tag and incorrect message ID

--- commands/edit.js
+++ commands/edit.js
@@ -3,38 +3,54 @@ module.exports = {
     category: 'Utility',
     description: 'Edits a message by its ID',
     requiredPermissions: ['ADMINISTRATOR'],
-    expectedArgs: '<channelID> <messageID> <newContent>',
-    // callback: async ({ channel, message, args }) => {
-
-    //     // await message.guild.channels.cache.get(`${args[0]}`).messages.fetch(`${args[1]}`)
-    //     await message.guild.channels.cache.get('844986163869646892').messages.fetch('1051950083207348304')
-
-    //     channel.messages.fetch(`${args[1]}`)
-    //         .then(message => {
-    //             console.log(message.content)
-    //             message.edit(args[2])
-    //                 .then(message => console.log(`Updated the content of the message to "${args[2]}"`))
-    //         })
-    //         .catch(console.error)
-
-    // }
+    expectedArgs: '<channelTag> <messageID>',
+    minArgs: 2,
+    maxArgs: 2,
     callback: ({ message, args }) => {
         //Get message id
         const messageID = args[1];
 
-        // Get the channel that the message is in
-        const channel = message.guild.channels.cache.get(args[0]);
+        // Get the channel that the message is in through its channel tag
+        const { mentions, channel } = message
+
+        const targetChannel = mentions.channels.first()
+        if (!targetChannel) {
+            message.reply('Please tag a channel to send the message to.')
+            return
+        }
 
         // Use the `fetchMessage` method to get the message by its ID
-        channel.messages.fetch(messageID)
+        targetChannel.messages.fetch(messageID)
         .then(fetchedMessage => {
             // Do something with the fetched message
-            console.log(fetchedMessage.content);
-            fetchedMessage.edit(args[2]);
+            
+            message.reply('Please enter the edited message.')
+            
+            // Get the edited message
+            const filter = (newMessage) => {
+                return newMessage.author.id === message.author.id
+            }
+    
+            const collector = channel.createMessageCollector({ filter, 
+                max: 1,
+                time: 1000 * 30, //30 seconds before time out
+            })
+    
+            collector.on('end', async (collected) => {
+                const collectedMessage = collected.first()
+                if (!collectedMessage) {
+                    message.reply('Message not received.')
+                    return
+                }
+                // Edit the message
+                fetchedMessage.edit(collectedMessage.content);
+                message.reply(`Message edited.`);
+            })
         })
         .catch(error => {
             // Log any errors that occur
             console.error(error);
+            message.reply("Message doesn't exist.")
         });
     }
 }
